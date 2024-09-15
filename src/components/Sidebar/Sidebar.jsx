2f718efd5933c6afcf7f11dd63dbe19704b60f48
ashutoshpythoncs@gmail.com <ashutoshpythoncs@gmail.com>
2024-09-16 01:09:39 +0530

    DevDisplay - OpenSource

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -57,6 +57,13 @@ function Sidebar() {
           </button>
         </a>
       </div>
+      <div className="pt-6">
+        <a href="https://ai.google.dev/competition/projects/helpmate-ai" target="_blank" rel="noreferrer">
+          <button className="inline-block cursor-pointer rounded-lg border-2 border-textSecondary bg-textSecondary px-[15px] py-1.5 text-center font-poppoins text-sm transition-all duration-500 hover:bg-transparent hover:text-textSecondary dark:text-white mr-4">
+          Community Spotlight
+          </button>
+        </a>
+      </div>
     </div>
   );
 }
