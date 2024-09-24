bf403e5cd09dc04c4814821edf0415435024906b
ashutoshpythoncs@gmail.com <ashutoshpythoncs@gmail.com>
2024-09-24 15:27:26 +0530

    DevDisplay - Open Source

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -73,6 +73,13 @@ function Sidebar() {
           </button>
         </a>
       </div>
+      <div className="pt-7">
+        <a href="#" target="_blank" rel="noreferrer">
+          <button className="mr-4 inline-block cursor-pointer rounded-lg border-2 border-textSecondary bg-textSecondary px-[15px] py-1.5 text-center font-poppoins text-sm transition-all duration-500 hover:bg-transparent hover:text-textSecondary dark:text-white">
+          Tech Opportunities Hub 🚀
+          </button>
+        </a>
+      </div>
     </div>
   );
 }
