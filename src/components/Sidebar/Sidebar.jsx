d6f8f260b5220feb15bdb4da16ccdfd3c14ff4e3
ashutoshpythoncs@gmail.com <ashutoshpythoncs@gmail.com>
2024-10-03 05:20:37 +0530

    DevDisplay - Open Source

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -72,7 +72,7 @@ function Sidebar() {
           Spotlight
           </button>
         </a>
-        <a href="#" target="_blank" rel="noreferrer">
+        <a href="/src/Page/Opportunities.jsx" target="_blank" rel="noreferrer">
           <button className="mr-4 inline-block cursor-pointer rounded-lg border-2 border-textSecondary bg-textSecondary px-[15px] py-1.5 text-center font-poppoins text-sm transition-all duration-500 hover:bg-transparent hover:text-textSecondary dark:text-white">
             Opportunities Hub
           </button>
