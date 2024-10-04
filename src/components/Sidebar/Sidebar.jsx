ddfa7e13cf866386b928cf57600197c57d4ce6cb
ashutoshpythoncs@gmail.com <ashutoshpythoncs@gmail.com>
2024-10-04 22:20:46 +0530

    DevDisplay - Open Source

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,33 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import { FaLinkedin } from 'react-icons/fa';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCode, faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
-import { useNavigate } from 'react-router-dom';
 
 function Sidebar() {
-  const [theme, setTheme] = useState(() => {
-    return document.documentElement.classList.contains('dark') ? 'dark' : 'light';
-  });
-
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const htmlElement = document.documentElement;
-    if (theme === 'dark') {
-      htmlElement.classList.add('dark');
-      htmlElement.classList.remove('light');
-    } else {
-      htmlElement.classList.add('light');
-      htmlElement.classList.remove('dark');
-    }
-  }, [theme]);
+  const [theme, setTheme] = useState('dark');
 
   function toggleTheme() {
-    setTheme((prevTheme) => (prevTheme === 'dark' ? 'light' : 'dark'));
-  }
+    const htmlElement = document.documentElement;
+    const isDarkModeEnabled = htmlElement.classList.contains('dark');
 
-  function handleOpportunities() {
-    navigate('/opportunities');
+    if (isDarkModeEnabled) {
+      htmlElement.classList.remove('dark');
+      setTheme('light');
+    } else {
+      htmlElement.classList.add('dark');
+      setTheme('dark');
+    }
   }
 
   return (
@@ -48,47 +36,26 @@ function Sidebar() {
             className="h-10 w-10 cursor-pointer rounded-lg border-2 border-borderSecondary bg-white transition-all hover:border-textSecondary hover:text-textSecondary dark:border-borderColor dark:bg-textPrimary dark:text-white dark:hover:border-textSecondary dark:hover:text-textSecondary"
             onClick={toggleTheme}
           >
-            {theme === 'dark' ? (
-              <FontAwesomeIcon icon={faSun} fontSize="1rem" />
-            ) : (
+            {theme === 'light' ? (
               <FontAwesomeIcon icon={faMoon} fontSize="1rem" />
+            ) : (
+              <FontAwesomeIcon icon={faSun} fontSize="1rem" />
             )}
           </button>
         </div>
       </div>
-      <div className="text-secondaryColor dark:text-white">
-        Open Source community where you can discover, connect, collab with skilled developers, share your ideas then
-        build projects and also promote the project through this community.
-      </div>
+      <div className="text-secondaryColor dark:text-white">Discover and Connect with Skilled Developers.</div>
       <div className="pt-5">
-        <a
-          href="https://github.com/codeaashu/DevDisplay?tab=readme-ov-file#how-to-add-your-profile-"
-          target="_blank"
-          rel="noreferrer"
-        >
+        <a href="https://github.com/codeaashu/DevDisplay#how-to-add-your-profile-" target="_blank" rel="noreferrer">
           <button className="mr-4 inline-block cursor-pointer rounded-lg border-2 border-textSecondary bg-textSecondary px-[15px] py-1.5 text-center font-poppoins text-sm transition-all duration-500 hover:bg-transparent hover:text-textSecondary dark:text-white">
             Add your profile
           </button>
         </a>
         <a href="https://www.linkedin.com/company/devdisplay/" target="_blank" rel="noreferrer">
-          <button className="inline-flex cursor-pointer items-center rounded-lg border-2 border-textSecondary bg-textSecondary px-[15px] py-1.5 text-center font-poppoins text-sm transition-all duration-500 hover:bg-transparent hover:text-textSecondary dark:text-white">
-            <span>Connect</span>
-            <FaLinkedin className="text-1xl text-black-600 ml-2 duration-300 hover:scale-125" />
+          <button className="inline-block cursor-pointer rounded-lg border-2 border-textSecondary bg-textSecondary px-[15px] py-1.5 text-center font-poppoins text-sm transition-all duration-500 hover:bg-transparent hover:text-textSecondary dark:text-white">
+            Connect us
           </button>
         </a>
-      </div>
-      <div className="pt-6">
-        <a href="https://ai.google.dev/competition/projects/helpmate-ai" target="_blank" rel="noreferrer">
-          <button className="mr-4 inline-block cursor-pointer rounded-lg border-2 border-textSecondary bg-textSecondary px-[15px] py-1.5 text-center font-poppoins text-sm transition-all duration-500 hover:bg-transparent hover:text-textSecondary dark:text-white">
-            Spotlight
-          </button>
-        </a>
-        <button
-          onClick={handleOpportunities}
-          className="mr-4 inline-block cursor-pointer rounded-lg border-2 border-textSecondary bg-textSecondary px-[15px] py-1.5 text-center font-poppoins text-sm transition-all duration-500 hover:bg-transparent hover:text-textSecondary dark:text-white"
-        >
-          Opportunities Hub
-        </button>
       </div>
     </div>
   );
