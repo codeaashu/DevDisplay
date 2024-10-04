6b408e4b1c25f4f08d1ce81d31851b8bb9e94c31
Subash Chandra Bose <143197207+Bgmi10@users.noreply.github.com>
2024-10-04 05:47:14 +0000

    updated with user system theme by default

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,31 +1,45 @@
-import {  createContext, useEffect, useState } from "react"
-
+import React, { createContext, useState, useEffect } from 'react';
 
 export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const getInitialTheme = () => {
+    const savedTheme = localStorage.getItem('theme');
     
-
-export const ThemeProvider = ({children}) => {
-
-    const [theme, setTheme] = useState(() => {
-        const storedvalue = localStorage.getItem('theme');
-        return storedvalue ? storedvalue : 'light';
-    })
-
-    useEffect(() => {
-        localStorage.setItem('theme', theme);
-    },[theme])
-
-    const toggletheme = () => {
-        setTheme(prev => prev === 'light' ? 'dark' : 'light')
+    if (savedTheme) {
+      return savedTheme; 
+    } else {
+      const systemPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+      return systemPrefersDark ? 'dark' : 'light'; 
     }
+  };
+
+ 
+  const [theme, setTheme] = useState(getInitialTheme);
+
+ 
+  useEffect(() => {
+    localStorage.setItem('theme', theme); 
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [theme]);
+
   
-   
-    return (
+  const toggletheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
 
-        <ThemeContext.Provider  value={{theme, toggletheme}}>
-            {children}
-        </ThemeContext.Provider>
+  const setSystemTheme = () => {
+    const systemPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    setTheme(systemPrefersDark ? 'dark' : 'light');
+  };
 
-    )
-
-}
+  return (
+    <ThemeContext.Provider value={{ theme, toggletheme, setSystemTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
