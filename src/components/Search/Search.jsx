a97055a8cb3f3bf7c207a15cdec98b1856f0d303
Siddhant <siddhantgureja39@gmail.com>
2024-10-23 01:35:20 +0530

    Added voice search functionality allowing users to serach people without typing and added skills dropdown menu, allowing users to select a skill without typing

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,13 +1,74 @@
 import React, { useState, useRef, useEffect } from 'react';
 import useDebounce from '../../hooks/useDebouncer';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faMagnifyingGlass, faXmark, faMicrophone } from '@fortawesome/free-solid-svg-icons';
 
 function Search({ onSearch }) {
   const [searchValue, setSearchValue] = useState('');
   const [prevSearchValue, setPrevSearchValue] = useState('');
   const [searchCriteria, setSearchCriteria] = useState('name');
   const searchInput = useRef(null);
+  const [isListening, setIsListening] = useState(false);
+  const [skills, setSkills] = useState([
+    'React',
+    'Redux',
+    'HTML',
+    'CSS',
+    'Tailwind',
+    'Django',
+    'TypeScript',
+    'Figma',
+    'UI',
+    'UX',
+    'C',
+    'C++',
+    'JavaScript',
+    'Python',
+    'Java',
+    'Web Development',
+    'DBMS',
+    'Operating Systems',
+    'Computer Networks',
+    'Machine Learning',
+    'Deep Learning',
+    'Flutter',
+    'Hugging Face',
+    'NLP',
+    'REST APIs',
+    'GraphQL',
+    'Docker',
+    'Kubernetes',
+    'Firebase',
+    'PostgreSQL',
+    'MongoDB',
+    'MySQL',
+    'Node.js',
+    'Express.js',
+    'Git',
+    'GitHub',
+    'TensorFlow',
+    'PyTorch',
+    'Azure',
+    'AWS',
+    'Google Cloud',
+    'Linux',
+    'Cybersecurity',
+    'Microservices',
+    'Blockchain',
+    'Jenkins',
+    'CI/CD',
+    'TDD',
+    'Agile Methodologies',
+    'Data Structures',
+    'Algorithms',
+    'Computer Vision',
+    'Natural Language Processing',
+    'UI Design',
+    'UX Design',
+    'Cloud Computing',
+    'Socket Programming',
+    'Data Mining',
+  ]);
 
   const handleInputChange = (event) => {
     setSearchValue(event.target.value);
@@ -15,6 +76,7 @@ function Search({ onSearch }) {
 
   const handleCriteriaChange = (event) => {
     setSearchCriteria(event.target.value);
+    setSearchValue('');
   };
 
   const debouncedValue = useDebounce(searchValue, 500);
@@ -57,6 +119,37 @@ function Search({ onSearch }) {
     searchInput.current.focus();
   }, []);
 
+  const handleVoiceSearch = () => {
+    if ('webkitSpeechRecognition' in window) {
+      const recognition = new window.webkitSpeechRecognition();
+      recognition.continuous = false;
+      recognition.interimResults = false;
+
+      recognition.onstart = () => {
+        setIsListening(true);
+      };
+
+      recognition.onresult = (event) => {
+        const transcript = event.results[0][0].transcript;
+        setSearchValue(transcript);
+        handleSearch();
+      };
+
+      recognition.onerror = (event) => {
+        console.error('Speech recognition error', event.error);
+        setIsListening(false);
+      };
+
+      recognition.onend = () => {
+        setIsListening(false);
+      };
+
+      recognition.start();
+    } else {
+      alert('Speech recognition is not supported in your browser.');
+    }
+  };
+
   return (
     <div className="relative flex items-center justify-end space-x-4 pb-6">
       <select
@@ -69,28 +162,50 @@ function Search({ onSearch }) {
         <option value="skill">Skill</option>
       </select>
       <div className="relative w-full">
-        <input
-          className="focus:border-primaryFocus focus:bg-primaryLight dark:focus:border-secondaryFocus dark:focus:bg-secondaryLight h-12 w-full rounded-lg border-2 border-borderSecondary bg-primaryColor px-4 py-3 pr-12 font-spaceMono text-base text-secondaryColor outline-none dark:border-borderColor dark:bg-secondaryColor dark:text-white"
-          ref={searchInput}
-          type="text"
-          onChange={handleInputChange}
-          value={searchValue}
-          placeholder={`Search user by ${searchCriteria}`}
-          onKeyDown={handleSearchOnEnter}
-        />
+        {searchCriteria === 'skill' ? (
+          <select
+            className="focus:border-primaryFocus focus:bg-primaryLight dark:focus:border-secondaryFocus dark:focus:bg-secondaryLight h-12 w-full rounded-lg border-2 border-borderSecondary bg-primaryColor px-4 py-3 pr-12 font-spaceMono text-base text-secondaryColor outline-none dark:border-borderColor dark:bg-secondaryColor dark:text-white"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          >
+            <option value="">Select a skill</option>
+            {skills.map((skill, index) => (
+              <option key={index} value={skill}>
+                {skill}
+              </option>
+            ))}
+          </select>
+        ) : (
+          <input
+            className="focus:border-primaryFocus focus:bg-primaryLight dark:focus:border-secondaryFocus dark:focus:bg-secondaryLight h-12 w-full rounded-lg border-2 border-borderSecondary bg-primaryColor px-4 py-3 pr-12 font-spaceMono text-base text-secondaryColor outline-none dark:border-borderColor dark:bg-secondaryColor dark:text-white"
+            ref={searchInput}
+            type="text"
+            onChange={handleInputChange}
+            value={searchValue}
+            placeholder={`Search user by ${searchCriteria}`}
+            onKeyDown={handleSearchOnEnter}
+          />
+        )}
         {searchValue ? (
           <FontAwesomeIcon
             onClick={handleDeleteButtonClick}
-            className="hover:text-primaryFocus dark:hover:text-secondaryFocus absolute right-4 top-1/2 -translate-y-1/2 scale-125 transform cursor-pointer text-xl text-secondaryColor dark:text-white"
+            className="hover:text-primaryFocus dark:hover:text-secondaryFocus absolute right-12 top-1/2 -translate-y-1/2 scale-125 transform cursor-pointer text-xl text-secondaryColor dark:text-white"
             icon={faXmark}
           />
         ) : (
           <FontAwesomeIcon
             onClick={handleSearchButtonClick}
-            className="hover:text-primaryFocus dark:hover:text-secondaryFocus absolute right-4 top-1/2 -translate-y-1/2 transform cursor-pointer text-xl text-secondaryColor dark:text-white"
+            className="hover:text-primaryFocus dark:hover:text-secondaryFocus absolute right-12 top-1/2 -translate-y-1/2 transform cursor-pointer text-xl text-secondaryColor dark:text-white"
             icon={faMagnifyingGlass}
           />
         )}
+        <FontAwesomeIcon
+          onClick={handleVoiceSearch}
+          className={`hover:text-primaryFocus dark:hover:text-secondaryFocus absolute right-4 top-1/2 -translate-y-1/2 transform cursor-pointer text-xl ${
+            isListening ? 'text-red-500' : 'text-secondaryColor dark:text-white'
+          }`}
+          icon={faMicrophone}
+        />
       </div>
     </div>
   );
