b185d700692dd8ac7043ad9030defdd705747260
ashutoshpythoncs@gmail.com <ashutoshpythoncs@gmail.com>
2024-11-03 18:39:54 +0530

    Updates SEO

--- src/App.js
+++ src/App.js
@@ -8,22 +8,22 @@ function App() {
   return (
     <BrowserRouter>
       <Helmet>
-        <title>DevDisplay - Your Global Open Source Community Platform</title>
+        <title>DevDisplay - Global Open Source Community Platform</title>
         <meta
           name="description"
-          content="DevDisplay brings together all your tech needs in one place. Showcase your skills, connect with collaborators, and join a user-friendly, searchable platform."
+          content="DevDisplay brings together all your tech needs in one place. Showcase your skills, connect, collaborate, build and promote."
         />
         <meta
           name="keywords"
-          content="DevDisplay, open source, community, tech skills, collaboration, tech platform, developers"
+          content="DevDisplay, open source, community, tech skills, collaboration, tech platform, developers, devdisplay, devdisplay org, developers, opensource, developer community, tech community"
         />
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        <meta property="og:title" content="DevDisplay - Your Global Open Source Community Platform" />
+        <meta property="og:title" content="DevDisplay - Global Open Source Community Platform" />
         <meta
           property="og:description"
-          content="Join DevDisplay to showcase your skills and connect with potential collaborators, all in a user-friendly, searchable format."
+          content="Join DevDisplay to Showcase your skills, connect, collaborate, build and promote."
         />
         <meta property="og:image" content="https://www.devdisplay.org/og-image.jpg" />
         <meta property="og:url" content="https://www.devdisplay.org" />
@@ -37,18 +37,18 @@ function App() {
           content="Showcase your skills, connect with collaborators, and join a user-friendly platform for developers worldwide."
         />
         <meta name="twitter:image" content="https://www.devdisplay.org/twitter-image.jpg" />
-        <meta name="twitter:site" content="@devdisplay" />
+        <meta name="twitter:site" content="@devdisplay_" />
 
         <meta name="robots" content="index, follow" />
 
         <link rel="canonical" href="https://www.devdisplay.org" />
 
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/devDisplayLOGO.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 
         <meta name="theme-color" content="#317EFB" />
 
-        <meta name="author" content="DevDisplay Team" />
+        <meta name="author" content="Ashutosh Singh" />
 
         <meta property="og:locale" content="en_US" />
       </Helmet>
