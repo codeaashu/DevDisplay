6c2b6ea306eb620b682c82421cfeee94336e5133
ashutoshpythoncs@gmail.com <ashutoshpythoncs@gmail.com>
2024-11-11 01:13:40 +0530

    update

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Globe from '../components/Globe';
 import { Star } from '@mui/icons-material';
