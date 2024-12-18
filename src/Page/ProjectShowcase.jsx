a23e8937fc3af92409f28899dfed52cb5169c6c6
ashutoshpythoncs@gmail.com <ashutoshpythoncs@gmail.com>
2024-12-18 19:44:44 +0530

    update

--- src/Page/ProjectShowcase.jsx
+++ src/Page/ProjectShowcase.jsx
@@ -8,10 +8,12 @@ const ProjectsPage = () => {
           This is the project Showcase page - Want to Build this page as a contributer
         </h1>
       </header>
-      <h1 className="text-2xl font-bold">Click here to contribute on this features 👇🏻</h1>
+      <h1 className="text-2xl font-bold">Click here to check out the feature details 👇🏻</h1>
       <a href="https://github.com/codeaashu/DevDisplay/issues/571#issue-2678911438" target="_blank" rel="noreferrer">
         <button className="inline-flex cursor-pointer items-center rounded-lg border-2 border-textSecondary bg-textSecondary px-[15px] py-1.5 text-center font-poppoins text-sm transition-all duration-500 hover:bg-transparent hover:text-textSecondary dark:text-white">
-          <span>Feature Details</span>
+          <span>
+            <b>🌟 Add Project Showcase Features 💌 & Get 8 Benefits 🌟</b>
+          </span>
         </button>
       </a>
     </div>
