80c4124d41e28356aae087cee3f5646318192e19
Vighnesh Brahme <v8ganesh@gmail.com>
2025-01-04 19:06:28 +0530

    added user model
    (cherry picked from commit d50b03247efd544ff370634f7683d48284e105b7)

--- backend/constants.js
+++ backend/constants.js
@@ -4,6 +4,16 @@ const Constants = {
   MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost:27017/devdisplay',
   JSON_LIMIT: '50mb',
   ENV: process.env.NODE_ENV || 'development',
+  API_BASE: '/api/v1',
+
+  // constraints
+  USERNAME_MINLEN: 3,
+  USERNAME_MAXLEN: 20,
+  BIO_MAXLEN: 200,
+
+  // JWT
+  // REFRESH_TOKEN_MAXAGE: 60 * 60 * 24 * 7, // 1 week
+  // ACCESS_TOKEN_MAXAGE: 60 * 20, // 20 minutes
 };
 
 export default Constants;
