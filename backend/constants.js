40cbf5bb4d25b699f0120596c7525dcd21d8b47d
Vighnesh Brahme <v8ganesh@gmail.com>
2025-01-04 11:40:38 +0530

    added helpers and health route
    (cherry picked from commit 6c6a201ba210ac41704970b4eb8b6e11d5a56d22)

--- backend/constants.js
+++ backend/constants.js
@@ -1,4 +1,4 @@
-const CONSTANTS = {
+const Constants = {
   PORT: process.env.PORT || 5000,
   ORIGIN_URL: process.env.ORIGIN_URL || 'http://localhost:3000',
   MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost:27017/devdisplay',
@@ -6,4 +6,4 @@ const CONSTANTS = {
   ENV: process.env.NODE_ENV || 'development',
 };
 
-export default CONSTANTS;
+export default Constants;
---
+++ backend/helpers/ApiResponse.js
@@ -0,0 +1,39 @@
+// standardized API response format
+import Constants from "../constants.js";
+
+class ApiResponse {
+  constructor(status, message, data = null, errorType = 'UNSPECIFIED', error = null) {
+    this.success = status < 300 ? true : false; // Boolean value indicating whether the request was successful or not
+    this.message = message, // Brief description of the result
+    this.data = data || null, // Contains the actual response data (if any)
+    this.status = status || 200 // HTTP status code
+
+    // If the environment is development, include the error details in the response
+    // POLICY DECISION ?? Only include error details in response if in development mode
+    this.error = Constants.ENV === 'development' ? error : null;
+    this.errorType = Constants.ENV === 'development' ? errorType : null;
+
+    // Log the response details to the console
+    // DEVELOPMENT ONLY
+    if (Constants.ENV === 'development') {
+      console.log("[API RESPONSE START]");
+      console.log(`[API RESPONSE] ${this.success ? 'SUCCESS' : 'FAILURE'}`);
+      console.log(`[API RESPONSE] STATUS: ${this.status}`);
+      console.log(`[API RESPONSE] MESSAGE: ${this.message}`);
+      console.log(`[API RESPONSE] DATA: ${JSON.stringify(this.data)}`);
+      console.log(`[API RESPONSE] ERROR: ${JSON.stringify(this.error)}`);
+      console.log(`[API RESPONSE] ERROR TYPE: ${this.errorType}`);
+      console.log(`[API RESPONSE] TIMESTAMP: ${new Date().toISOString()}`);
+      console.log("[API RESPONSE END]");
+    }
+  }
+}
+
+// Helper functions to send API responses
+const successResponse = (res, data, message) => {
+  res.status(200).json(new ApiResponse(200, message, data));
+}
+const errorResponse = (res, status, message, errorType, error) => {
+  res.status(status).json(new ApiResponse(status, message, null, errorType, error));
+}
+export { successResponse, errorResponse };
