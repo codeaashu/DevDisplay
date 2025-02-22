396e80d10e9d71bb27dded3250e2556e1c1294ac
ashutoshpythoncs@gmail.com <ashutoshpythoncs@gmail.com>
2025-02-22 18:06:32 +0530

    GLOBAL

--- src/components/Globe.jsx
+++ src/components/Globe.jsx
@@ -42,57 +42,57 @@ function Globe() {
       markerColor: [0.1, 0.8, 1],
       glowColor: [1, 1, 1],
       markers: [
-        { location: [37.7595, -122.4367], size: 0.03 }, // San Francisco
-        { location: [40.7128, -74.006], size: 0.1 }, // New York
-        { location: [1.3521, 103.8198], size: 0.05 }, // Singapore
-        { location: [35.8617, 104.1954], size: 0.1 }, // China
-        { location: [-14.235, -51.9253], size: 0.1 }, // Brazil
-        { location: [30.3753, 69.3451], size: 0.05 }, // Pakistan
-        { location: [28.7041, 77.1025], size: 0.05 }, // Delhi
-        { location: [19.076, 72.8777], size: 0.05 }, // Maharashtra
-        { location: [13.0827, 80.2707], size: 0.05 }, // Tamil Nadu
-        { location: [22.5726, 88.3639], size: 0.05 }, // West Bengal
-        { location: [12.9716, 77.5946], size: 0.05 }, // Karnataka
-        { location: [17.385, 78.4867], size: 0.03 }, // Telangana
-        { location: [23.2599, 77.4126], size: 0.02 }, // Madhya Pradesh
-        { location: [26.9124, 75.7873], size: 0.02 }, // Rajasthan
-        { location: [21.1702, 72.8311], size: 0.02 }, // Gujarat
-        { location: [11.0168, 76.9558], size: 0.02 }, // Kerala
-        { location: [15.2993, 74.124], size: 0.02 }, // Goa
-        { location: [25.3176, 82.9739], size: 0.04 }, // Uttar Pradesh
-        { location: [27.0238, 74.2179], size: 0.03 }, // Haryana
-        { location: [30.7333, 76.7794], size: 0.02 }, // Punjab
-        { location: [31.1048, 77.1734], size: 0.03 }, // Himachal Pradesh
+        { location: [37.7595, -122.4367], size: 0.01 }, // San Francisco
+        { location: [40.7128, -74.006], size: 0.01 }, // New York
+        { location: [1.3521, 103.8198], size: 0.01 }, // Singapore
+        { location: [35.8617, 104.1954], size: 0.01 }, // China
+        { location: [-14.235, -51.9253], size: 0.01 }, // Brazil
+        { location: [30.3753, 69.3451], size: 0.01 }, // Pakistan
+        { location: [28.7041, 77.1025], size: 0.01 }, // Delhi
+        { location: [19.076, 72.8777], size: 0.01 }, // Maharashtra
+        { location: [13.0827, 80.2707], size: 0.01 }, // Tamil Nadu
+        { location: [22.5726, 88.3639], size: 0.01 }, // West Bengal
+        { location: [12.9716, 77.5946], size: 0.01 }, // Karnataka
+        { location: [17.385, 78.4867], size: 0.01 }, // Telangana
+        { location: [23.2599, 77.4126], size: 0.01 }, // Madhya Pradesh
+        { location: [26.9124, 75.7873], size: 0.01 }, // Rajasthan
+        { location: [21.1702, 72.8311], size: 0.01 }, // Gujarat
+        { location: [11.0168, 76.9558], size: 0.01 }, // Kerala
+        { location: [15.2993, 74.124], size: 0.01 }, // Goa
+        { location: [25.3176, 82.9739], size: 0.01 }, // Uttar Pradesh
+        { location: [27.0238, 74.2179], size: 0.01 }, // Haryana
+        { location: [30.7333, 76.7794], size: 0.01 }, // Punjab
+        { location: [31.1048, 77.1734], size: 0.01 }, // Himachal Pradesh
         { location: [34.0837, 74.7973], size: 0.01 }, // Jammu and Kashmir
-        { location: [15.9129, 79.74], size: 0.05 }, // Andhra Pradesh
-        { location: [22.9734, 78.6569], size: 0.03 }, // Chhattisgarh
-        { location: [23.6102, 85.2799], size: 0.02 }, // Jharkhand
-        { location: [20.9517, 85.0985], size: 0.02 }, // Odisha
-        { location: [25.0961, 85.3131], size: 0.05 }, // Bihar
-        { location: [24.6637, 93.9063], size: 0.05 }, // Manipur
-        { location: [27.533, 88.5122], size: 0.05 }, // Sikkim
-        { location: [26.2006, 92.9376], size: 0.05 }, // Assam
+        { location: [15.9129, 79.74], size: 0.01 }, // Andhra Pradesh
+        { location: [22.9734, 78.6569], size: 0.01 }, // Chhattisgarh
+        { location: [23.6102, 85.2799], size: 0.01 }, // Jharkhand
+        { location: [20.9517, 85.0985], size: 0.01 }, // Odisha
+        { location: [25.0961, 85.3131], size: 0.01 }, // Bihar
+        { location: [24.6637, 93.9063], size: 0.01 }, // Manipur
+        { location: [27.533, 88.5122], size: 0.01 }, // Sikkim
+        { location: [26.2006, 92.9376], size: 0.01 }, // Assam
         { location: [23.1645, 92.9376], size: 0.01 }, // Tripura
         { location: [25.467, 91.3662], size: 0.01 }, // Meghalaya
         { location: [27.0238, 93.6053], size: 0.01 }, // Arunachal Pradesh
         { location: [25.5705, 91.8801], size: 0.01 }, // Mizoram
-        { location: [24.517, 93.953], size: 0.05 }, // Nagaland
-        { location: [11.9416, 79.8083], size: 0.05 }, // Puducherry
-        { location: [10.8505, 76.2711], size: 0.05 }, // Lakshadweep
-        { location: [8.0883, 77.5385], size: 0.05 }, // Andaman and Nicobar Islands
-        { location: [9.082, 8.6753], size: 0.05 }, // Nigeria
-        { location: [28.3949, 84.124], size: 0.05 }, // Nepal
-        { location: [7.8731, 80.7718], size: 0.05 }, // Sri Lanka
-        { location: [23.685, 90.3563], size: 0.05 }, // Bangladesh
-        { location: [33.9391, 67.71], size: 0.05 }, // Afghanistan
-        { location: [51.5074, -0.1278], size: 0.05 }, // England
-        { location: [55.7558, 37.6173], size: 0.05 }, // Russia
-        { location: [25.2048, 55.2708], size: 0.05 }, // UAE
-        { location: [34.0522, -118.2437], size: 0.05 }, // Los Angeles
-        { location: [41.8781, -87.6298], size: 0.05 }, // Chicago
-        { location: [29.7604, -95.3698], size: 0.05 }, // Houston
-        { location: [33.4484, -112.074], size: 0.05 }, // Phoenix
-        { location: [39.7392, -104.9903], size: 0.05 }, // Denver
+        { location: [24.517, 93.953], size: 0.01 }, // Nagaland
+        { location: [11.9416, 79.8083], size: 0.01 }, // Puducherry
+        { location: [10.8505, 76.2711], size: 0.01 }, // Lakshadweep
+        { location: [8.0883, 77.5385], size: 0.01 }, // Andaman and Nicobar Islands
+        { location: [9.082, 8.6753], size: 0.01 }, // Nigeria
+        { location: [28.3949, 84.124], size: 0.01 }, // Nepal
+        { location: [7.8731, 80.7718], size: 0.01 }, // Sri Lanka
+        { location: [23.685, 90.3563], size: 0.01 }, // Bangladesh
+        { location: [33.9391, 67.71], size: 0.01 }, // Afghanistan
+        { location: [51.5074, -0.1278], size: 0.01 }, // England
+        { location: [55.7558, 37.6173], size: 0.01 }, // Russia
+        { location: [25.2048, 55.2708], size: 0.01 }, // UAE
+        { location: [34.0522, -118.2437], size: 0.01 }, // Los Angeles
+        { location: [41.8781, -87.6298], size: 0.01 }, // Chicago
+        { location: [29.7604, -95.3698], size: 0.01 }, // Houston
+        { location: [33.4484, -112.074], size: 0.01 }, // Phoenix
+        { location: [39.7392, -104.9903], size: 0.01 }, // Denver
       ],
       onRender: (state) => {
         state.phi = phi;
