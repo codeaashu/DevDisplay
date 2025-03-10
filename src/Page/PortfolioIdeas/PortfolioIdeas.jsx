3d802ea6bee71b562e1c433780c9b5e67d6a89af
ashutoshpythoncs@gmail.com <ashutoshpythoncs@gmail.com>
2025-03-11 00:00:15 +0530

    v2.0 Update

--- src/Page/PortfolioIdeas/PortfolioIdeas.jsx
+++ src/Page/PortfolioIdeas/PortfolioIdeas.jsx
@@ -118,7 +118,7 @@ const PortfolioIdeas = () => {
                 />
               </div>
               <div>
-                <label className="mb-1 block text-sm font-medium">Screenshot UR]L</label>
+                <label className="mb-1 block text-sm font-medium">Screenshot URL</label>
                 <input
                   type="url"
                   value={newPortfolio.screenshot}
