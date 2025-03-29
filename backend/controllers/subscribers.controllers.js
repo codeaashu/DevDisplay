ed4c1bc5f2affc75f0a4bc46afb12c5a897ecbbc
Thakar-Advait <advaitthakar18@gmail.com>
2025-03-29 22:11:32 +0530

    added backend for IndustryTrends

---
+++ backend/controllers/subscribers.controllers.js
@@ -0,0 +1,10 @@
+import Subscribers from "../models/subscribers.models.js";
+
+export const getSubscribers = async (req, res) => {
+    try {
+        const subscribers = await Subscribers.find().where("status").equals("subscribed");
+        res.status(200).json(subscribers);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
