185227b2ed54cd329cf816142428c843a99242d9
ubaman34 <0707amanpatel2005@gmail.com>
2025-04-13 17:35:22 +0530

    updated

--- src/Page/OpportunitiesHub/CompetitionList.jsx
+++ src/Page/OpportunitiesHub/CompetitionList.jsx
@@ -54,6 +54,17 @@ const Competitions = [
     shareLink: '#comingsoon',
   },
 
+  {
+    organizer: 'Organizer Name',
+    title: 'Event Name',
+    location: 'Event Location',
+    date: 'Event Date',
+    domains: ['Open Innovation', 'No Restrictions'],
+    applyLink: '#',
+    poster: '/assets/Coming Soon.png',
+    shareLink: '#comingsoon',
+  },
+
 ];
 
 const StyledCompetitionsCard = styled.div`
