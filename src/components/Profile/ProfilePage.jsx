c3b6f3ee0f5ef745115bd96441146d9f822a49f6
ashutoshpythoncs@gmail.com <ashutoshpythoncs@gmail.com>
2025-09-12 13:13:15 +0530

    dev

--- src/components/Profile/ProfilePage.jsx
+++ src/components/Profile/ProfilePage.jsx
@@ -28,7 +28,6 @@ const ProfilePage = () => {
         }
         const response = await fetch(`/data/${profileFile}`);
         const data = await response.json();
-        // Add fileName (without .json) to the profile data
         setProfileData({ ...data, fileName: profileFile.replace('.json', '') });
       } catch (error) {
         console.error('Error fetching profile data:', error);
